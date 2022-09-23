85c4fb8454dcc129320b26d0a85cc254a8b5e428
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-09-23 03:36:29 +0300

    completed react hooks, state and events code along

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setState] = useState(false)
+
+  function onOff() {
+    setState((isOn) => !isOn)
+  }
+
+  const colour = isOn ? "green" : "red"
+
+  console.log(isOn)
+  return <button style={{background: colour}} onClick={onOff}>{isOn ? "On" : "Off"}</button>;
 }
 
 export default Toggle;
